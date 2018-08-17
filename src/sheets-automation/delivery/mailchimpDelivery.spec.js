f6906bbc8de39bd3a0bd72f1853906711928ca20
Kyle Galbraith <kyle.galbraith45999999999@gmail.com>
2018-08-16 21:33:43 -0700

    add istanbul and remaining tests

--- src/sheets-automation/delivery/mailchimpDelivery.spec.js
+++ src/sheets-automation/delivery/mailchimpDelivery.spec.js
@@ -8,6 +8,7 @@ describe('mailChimpDelivery module', () => {
     before(() => {
         let stubClient = sinon.createStubInstance(MailchimpClient);
         stubClient.post.withArgs('/templates').resolves({id: 123456});
+        stubClient.post.withArgs('/campaigns').resolves({id: "campaign-id"});
         this.mailChimpDelivery = new MailChimpDelivery(stubClient);
     });
 
@@ -17,8 +18,20 @@ describe('mailChimpDelivery module', () => {
         });
 
         it('should return an id field', () => {
-            this.mailChimpDelivery.saveTemplate(null, null).then((data) => {
+            this.mailChimpDelivery.saveTemplate("name", "markup").then((data) => {
                 expect(data.id).to.equal(123456);
+            });            
+        });
+    });
+
+    describe('createRegularCampaign', () => {
+        it('should be a function', () => {
+            expect(this.mailChimpDelivery.createRegularCampaign).to.be.a('function');
+        });
+
+        it('should return an id field', () => {
+            this.mailChimpDelivery.createRegularCampaign("listid", "title", "subject", "templateid").then((data) => {
+                expect(data.id).to.equal("campaign-id");
             });            
         });
     });
