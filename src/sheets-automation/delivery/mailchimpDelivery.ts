71968e4eabdf47eb5e1c34c6ef5223334a9a2c8a
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2019-10-02 22:36:59 -0400

    build: converted delivery service to typescript

---
+++ src/sheets-automation/delivery/mailchimpDelivery.ts
@@ -0,0 +1,37 @@
+import Mailchimp from "mailchimp-api-v3";
+
+export class MailchimpDelivery {
+  private readonly templateUrl = "/templates";
+  private readonly campaignsUrl = "/campaigns";
+  private mailChimpClient: Mailchimp;
+
+  constructor(mailChimpClient: Mailchimp) {
+    this.mailChimpClient = mailChimpClient;
+  }
+
+  saveTemplate(name: string, markup: string) {
+    return this.mailChimpClient.post(this.templateUrl, {
+      name: name,
+      html: markup
+    });
+  }
+
+  createRegularCampaign(
+    recipientListId: string,
+    title: string,
+    subjectLine: string,
+    templateId: number
+  ) {
+    return this.mailChimpClient.post(this.campaignsUrl, {
+      type: "regular",
+      recipients: {
+        list_id: recipientListId
+      },
+      settings: {
+        title: title,
+        subject_line: subjectLine,
+        template_id: templateId
+      }
+    });
+  }
+}
